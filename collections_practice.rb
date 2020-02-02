f5b87e5720918f1efd83fd605cd9155b71a989b1
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-02 00:20:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+  array.sort 
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end 
+
+def sort_array_char_count(array)
+  array.sort{|x,y| x.length <=> y.length} 
+end 
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1] 
+end 
+
+def reverse_array(array)
+  array.reverse 
+end 
+
+#def kesha_maker
+
+#end 
+
+def find_a(array)
+  array.select{|w| w.start_with?("a")}
+end 
+
+def sum_array(array)
+  array.sum 
+end 
+
+def add_s(array)
+  array.collect {|w| w << "s"}
+  if 
+    w != feet
+    return feet 
+  else w 
+    return w 
+  end 
+end 
